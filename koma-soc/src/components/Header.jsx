3645a8d443b48337d1474becbd4abe9d856c064d
Ivan <58357980+Ivan-Corporation@users.noreply.github.com>
2021-01-01 17:43:57 +0700

    v0.1

---
+++ koma-soc/src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://pngimg.com/uploads/bulb/bulb_PNG1250.png' alt='logo' />
+    </header>
+}
+
+export default Header;
---
+++ koma-soc/src/components/Profile.jsx
@@ -0,0 +1,25 @@
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='http://img.crazys.info/files/i/2012.5.22/1337696897_space-66.jpg' />
+        </div>
+        <div>
+            <img src='https://image.winudf.com/v2/image/Y29tLmVuZ2luYWlyLmNvb2xjYXR3YWxscGFwZXJfc2NyZWVuXzJfMTUyNzI5OTk2OF8wMDA/screen-2.jpg?fakeurl=1&type=.jpg' />
+        </div>
+        <div>
+            My posts
+      <div>
+                New post
+      </div>
+            <div>
+                post 1
+      </div>
+            <div>
+                post 2
+      </div>
+        </div>
+    </div>
+}
+
+export default Profile;
