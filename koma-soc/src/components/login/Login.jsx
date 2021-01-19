60dc937b77c529d1991b1d5e100aa18c1284dfbd
Ivan <58357980+Ivan-Corporation@users.noreply.github.com>
2021-01-19 13:16:20 +0700

    v0.19 react-final-form

--- koma-soc/src/components/login/Login.jsx
+++ koma-soc/src/components/login/Login.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { Form, Field } from 'react-final-form'
 
-const Login = (props) => {
-    return <h1>LOGIN</h1>
+
+const LoginForm = (props) => {
+    return (
+        <form>
+            <div>
+                <Field placeholder ={"Login"} component={"input"}/>
+            </div>
+            <div>
+            <Field placeholder ={"Password"} component={"input"}/>
+            </div>
+            <div>
+            <input type ={"checkbox"}/> Remember me
+            </div>
+            <div>
+                <button>Login  </button>
+            </div>
+        </form>
+        )
 }
 
-export default Login
+
+
+const Login = (props) => {
+    return <div>
+        <h1>Login</h1>
+        <LoginForm/>
+        </div>
+}
+
+export default Login;
